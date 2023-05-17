379d970f827f2234a64636bea64020f859a8bc1d
wizexx02 <dvladvlad11@gmail.com>
2023-05-17 16:00:23 +0300

    add

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
-  const { default: Swal } = require("sweetalert2");
+  // const { default: Swal } = require("sweetalert2");
 
-  document.getElementById('follow').addEventListener('click', function(){
-    Swal.fire('Any fool can use a computer')
+  // document.getElementById('follow').addEventListener('click', function(){
+  //   Swal.fire('Any fool can use a computer')
+  // })
+  const { value: url } = await Swal.fire({
+    input: 'url',
+    inputLabel: 'URL address',
+    inputPlaceholder: 'Enter the URL'
   })
+  
+  if (url) {
+    Swal.fire(`Entered URL: ${url}`)
+  }
   const swiper = new Swiper('.swiper', {
     // Optional parameters
     loop: true,
